5fe37d6d13a35403179f1dc306d05aa9c46d102d
mimi110 <121365429+mimi110@users.noreply.github.com>
2025-10-03 15:35:40 -0500

    New Layout

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-
 // Scrolling 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-        if (targetElement) {
-
-            targetElement.scrollIntoView({ behavior: 'smooth' });
-        }
-    });
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) targetElement.scrollIntoView({ behavior: 'smooth' });
+  });
 });
 
 // Type Text
-
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Designer", "Developer"];
+
+const textArray = ["developer", "designer"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; 
@@ -30,8 +26,7 @@ function type() {
     typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
     charIndex++;
     setTimeout(type, typingDelay);
-  } 
-  else {
+  } else {
     cursorSpan.classList.remove("typing");
     setTimeout(erase, newTextDelay);
   }
@@ -40,30 +35,26 @@ function type() {
 function erase() {
   if (charIndex > 0) {
     if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
     charIndex--;
     setTimeout(erase, erasingDelay);
-  } 
-  else {
+  } else {
     cursorSpan.classList.remove("typing");
-    textArrayIndex++;
-    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    textArrayIndex = (textArrayIndex + 1) % textArray.length;
     setTimeout(type, typingDelay + 1100);
   }
 }
 
 document.addEventListener("DOMContentLoaded", function() { 
-  if(textArray.length) setTimeout(type, newTextDelay + 250);
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
 });
 
 // Refresh 
-
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 };
 
 // Image
-
 function enlargeImage(img) {
   img.classList.toggle('enlarged');
 }
